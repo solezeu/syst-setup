46e7d1a6dbc42b53f34dfbbd011d73ff8c65cde2
solezeu <carolearista@gmail.com>
2022-06-13 07:17:27 -0400

    change to varibles.sh

---
+++ variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Description: script to implement variable concept
+# Author: carole
+# Date: june 2022
+NAME=Arista
+echo "$NAME is very hardworking guy"
+echo "the manager says, he will hire $NAME in his new team wherever he goes"
+echo "$NAME like to show off because the manager loves her"
+echo "$NAME will always be available when they need her"
+
+echo "Hello there your username is ${USER}"
+echo "Your user ID is ${UID}"
+echo "Your shell is ${SHELL}"
+echo "Your home directory is ${HOME}"
+echo "This server's hostname is ${HOSTNAME}"
